122179e10d963db497d3246057c63a236930e072
parag vaid <you@example.com>
2019-01-29 09:57:30 +0530

    All Models Completed

--- project_finder_web_app/models.py
+++ project_finder_web_app/models.py
@@ -88,3 +88,56 @@ class ProjectTeamRequest(models.Model):
     def reject(self):
         self.status = 'R'
         self.save()
+
+class Hackathon(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    hackathon_desc = models.TextField(max_length=500)
+    link = models.URLField()
+    hackathon_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
+class HackathonTeam(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    leader = models.ForeignKey(User, on_delete=models.CASCADE)
+    current_members = models.ManyToManyField(User, related_name="team_members")
+    hackathon = models.ForeignKey(Hackathon, on_delete=models.CASCADE)
+    vacancies = models.PositiveSmallIntegerField(default=3)
+    closed = models.BooleanField(default=False)
+    cut_off_date = models.DateField(default=timezone.now)
+    skills_required = models.ManyToManyField(Skill)
+
+    def __str__(self):
+        return self.name
+
+    def add_member(self, member):
+        self.current_members.add(member)
+        self.save()
+
+
+class HackathonTeamRequest(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(HackathonTeam, on_delete=models.CASCADE)
+    message = models.TextField(max_length=500)
+    skills = models.ManyToManyField(Skill)
+    create_date = models.DateField(default=timezone.now)
+    status_choices = (
+        ('A', 'Accepted'),
+        ('P', 'Pending'),
+        ('R', 'Rejected'),
+    )
+    status = models.CharField(max_length=1, choices=status_choices, default='P')
+
+    def __str__(self):
+        return self.sender.username + ' --> ' + self.team.name
+
+    def accept(self):
+        self.status = 'A'
+        self.save()
+
+    def reject(self):
+        self.status = 'R'
+        self.save()
